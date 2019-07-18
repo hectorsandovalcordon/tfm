f9d68cc913366ca730bd0541a4b5dc7b5fe7c383
hector <hectorsandovalcordon@gmail.com>
2019-07-18 09:25:12 +0200

    tfm

---
+++ js/paradas.js
@@ -0,0 +1,104 @@
+var paradas= {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.8394638008541195,
+          41.67003290904381
+        ]
+      },
+      "properties": {
+        "id": "tuzsa-240",
+        "title": "(240) AV STA ISABEL 9 / AV DE LA INDUSTRIA Líneas: N7, 32, N1, 28",
+        "lastUpdated": "2019-05-14T19:25:50Z",
+        "link": "http://www.urbanosdezaragoza.es/frm_esquemaparadatime.php?poste=240",
+        "icon": "//www.zaragoza.es/contenidos/iconos/bus.png",
+        "destinos": [
+          {
+            "linea": "32",
+            "destino": "BOMBARDA.",
+            "primero": "1 minutos.",
+            "segundo": "11 minutos."
+          },
+          {
+            "linea": "60",
+            "destino": "PUENTE DE SANTIAGO.",
+            "primero": "7 minutos.",
+            "segundo": "21 minutos."
+          },
+          {
+            "linea": "28",
+            "destino": "COSO.",
+            "primero": "26 minutos.",
+            "segundo": "56 minutos."
+          }
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.8351316668085454,
+          41.66750615139868
+        ]
+      },
+      "properties": {
+        "id": "tuzsa-244",
+        "title": "(244) AV SANTA ISABEL 59 Líneas: 32, N1",
+        "lastUpdated": "2019-05-14T19:25:17Z",
+        "link": "http://www.urbanosdezaragoza.es/frm_esquemaparadatime.php?poste=244",
+        "icon": "//www.zaragoza.es/contenidos/iconos/bus.png",
+        "destinos": [
+          {
+            "linea": "60",
+            "destino": "PUENTE DE SANTIAGO.",
+            "primero": "5 minutos.",
+            "segundo": "19 minutos."
+          },
+          {
+            "linea": "32",
+            "destino": "BOMBARDA.",
+            "primero": "9 minutos.",
+            "segundo": "18 minutos."
+          }
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.8288676558178605,
+          41.66690160591595
+        ]
+      },
+      "properties": {
+        "id": "tuzsa-248",
+        "title": "(248) AV SANTA ISABEL 141 Líneas: 32, N1",
+        "lastUpdated": "2019-05-14T19:24:40Z",
+        "link": "http://www.urbanosdezaragoza.es/frm_esquemaparadatime.php?poste=248",
+        "icon": "//www.zaragoza.es/contenidos/iconos/bus.png",
+        "destinos": [
+          {
+            "linea": "60",
+            "destino": "PUENTE DE SANTIAGO.",
+            "primero": "4 minutos.",
+            "segundo": "18 minutos."
+          },
+          {
+            "linea": "32",
+            "destino": "BOMBARDA.",
+            "primero": "8 minutos.",
+            "segundo": "17 minutos."
+          }
+        ]
+      }
+    }
+  ]
+}
